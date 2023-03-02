f1ecfc69b4084ab41a1dc7a640894b256e0742c0
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-03-02 17:03:34 +0800

    fix dependency error for  date and time picker

--- todo/src/components/addNewTodo.js
+++ todo/src/components/addNewTodo.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import Modal from "./modal";
 import { Bell, CalendarDay, Clock, Palette, X } from "react-bootstrap-icons";
-// import {
-//   DatePicker,
-//   TimePicker,
-//   MuiPickersUtilsProvider,
-// } from "@material-ui/pickers";
-// import DateFnsUtils from "@date-io/date-fns";
+import {
+  DatePicker,
+  TimePicker,
+  MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 function AddNewTodo() {
   const [showModal, setShowModal] = useState(false);
   const [text, setText] = useState("");
-  // const [day, setDay] = useState(new Date());
-  // const [time, setTime] = useState(new Date());
+  const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState(new Date());
 
   return (
     <div className="AddNewTodo">
@@ -20,53 +20,55 @@ function AddNewTodo() {
         <button onClick={() => setShowModal(true)}>+ New Todo</button>
       </div>
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        <form>
-          <div className="text">
-            <h3>Add new to do!</h3>
-            <input
-              type="text"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              placeholder="To do ..."
-              autoFocus
-            />
-          </div>
-          <div className="remind">
-            <Bell />
-            <p>Remind Me!</p>
-          </div>
-          <div className="pick-day">
-            <div className="title">
-              <CalendarDay />
-              <p>Choose a day</p>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <form>
+            <div className="text">
+              <h3>Add new to do!</h3>
+              <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="To do ..."
+                autoFocus
+              />
             </div>
-            Date-Picker
-          </div>
-          <div className="pick-time">
-            <div className="title">
-              <Clock />
-              <p>Choose time</p>
+            <div className="remind">
+              <Bell />
+              <p>Remind Me!</p>
             </div>
-            Time-Picker
-          </div>
-          <div className="pick-project">
-            <div className="title">
-              <Palette />
-              <p>Choose a project</p>
+            <div className="pick-day">
+              <div className="title">
+                <CalendarDay />
+                <p>Choose a day</p>
+              </div>
+              <DatePicker value={day} onChange={(day) => setDay(day)} />
             </div>
-            <div className="projects">
-              <div className="project active">personal</div>
-              <div className="project">work</div>
-              <div className="project">work</div>
+            <div className="pick-time">
+              <div className="title">
+                <Clock />
+                <p>Choose time</p>
+              </div>
+              <TimePicker value={time} onChange={(time) => setTime(time)} />
             </div>
-          </div>
-          <div className="cancel" onClick={() => setShowModal(false)}>
-            <X size="40" />
-          </div>
-          <div className="confirm">
-            <button>+ Add to do</button>
-          </div>
-        </form>
+            <div className="pick-project">
+              <div className="title">
+                <Palette />
+                <p>Choose a project</p>
+              </div>
+              <div className="projects">
+                <div className="project active">personal</div>
+                <div className="project">work</div>
+                <div className="project">work</div>
+              </div>
+            </div>
+            <div className="cancel" onClick={() => setShowModal(false)}>
+              <X size="40" />
+            </div>
+            <div className="confirm">
+              <button>+ Add to do</button>
+            </div>
+          </form>
+        </MuiPickersUtilsProvider>
       </Modal>
     </div>
   );
