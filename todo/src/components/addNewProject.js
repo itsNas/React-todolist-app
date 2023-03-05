284e12fa9db2fd200a11eef21f1e6daf8bdd059f
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-03-05 10:28:25 +0800

    add projects components

--- todo/src/components/addNewProject.js
+++ todo/src/components/addNewProject.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Plus } from "react-bootstrap-icons";
 
 function AddNewProject() {
-  return <div className="AddNewProject">Add new project</div>;
+  return (
+    <div className="AddNewProject">
+      <div className="add-button">
+        <span>
+          <Plus size={20} />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default AddNewProject;
--- todo/src/components/project.js
+++ todo/src/components/project.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Pencil, XCircle } from "react-bootstrap-icons";
 import RenameProject from "./renameProject";
+import Modal from "./modal";
 
-function Project() {
+function Project({ project, edit }) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="Project">
-      <RenameProject />
+      <div className="name">{project.name}</div>
+      <div className="btns">
+        {edit ? (
+          <div className="edit-delete">
+            <span className="edit" onClick={() => setShowModal(true)}>
+              <Pencil size={13} />
+            </span>
+            <span className="delete">
+              <XCircle size={13} />
+            </span>
+          </div>
+        ) : project.numOfTodos === 0 ? (
+          ""
+        ) : (
+          <div className="total-todos">{project.numOfTodos}</div>
+        )}
+      </div>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <RenameProject project={project} setShowModal={setShowModal} />
+      </Modal>
     </div>
   );
 }
